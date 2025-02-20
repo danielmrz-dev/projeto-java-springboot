7d7d31c8c702547b68649e5c35b0a0e20dcadd28
Daniel Moraes Mariz <danielmrz.dev@gmail.com>
2025-02-20 17:50:44 -0300

    Initial commit

---
+++ src/main/java/projeto/apirest/ProjetoJavaSpringbootApplication.java
@@ -0,0 +1,14 @@
+package projeto.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaSpringbootApplication.class, args);
+		System.out.println("Servidor rodando! 🔥");
+	}
+
+}
---
+++ src/test/java/projeto/apirest/ProjetoJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
